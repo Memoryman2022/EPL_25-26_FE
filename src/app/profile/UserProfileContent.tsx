5e9f0cea998047fc8f99e6d88a60f06a132d6086
Robert Cannon <cannon@newsaktuell.de>
2025-09-26 20:27:07 +0200

    user styling

--- src/app/profile/UserProfileContent.tsx
+++ src/app/profile/UserProfileContent.tsx
@@ -5,7 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 import { useUser } from "@/app/auth/components/Context";
 import { api } from "@/lib/api";
-import UserPredictionsList from "@/components/UserPredictionsList";
+import LeaderboardClient from "../leaderboard/LeaderboardClient";
 
 type User = {
   _id: string;
@@ -59,9 +59,11 @@ export default function UserProfile() {
   if (!user) return <p>User not found.</p>;
 
   return (
-    <div className="p-4">
-      <div className="user-profile max-w-md mx-auto p-4 bg-gray-800 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-4">{user.userName}</h2>
+    <div>
+      <div className="user-profile max-w-md mx-auto p-4 bg-gray-800  shadow-md">
+        <h2 className="text-2xl font-bold text-center mt-0 m-4">
+          {user.userName}
+        </h2>
         <div className="flex gap-4 items-start">
           <Image
             src={user.profileImage || "/default-profile.png"}
@@ -71,17 +73,17 @@ export default function UserProfile() {
             className="rounded-lg"
           />
           <div className="flex flex-col space-y-2">
-            <p>
-              <strong>Position:</strong> {user.position}
+            <p className="gap-10">
+              <strong>POSITION:</strong> {user.position}
             </p>
             <p>
-              <strong>Score:</strong> {user.score}
+              <strong>POINTS:</strong> {user.score}
             </p>
             <p>
-              <strong>Correct Scores:</strong> {user.correctScores}
+              <strong>SCORES:</strong> {user.correctScores}
             </p>
             <p>
-              <strong>Correct Outcomes:</strong> {user.correctOutcomes}
+              <strong>OUTCOMES:</strong> {user.correctOutcomes}
             </p>
           </div>
         </div>
@@ -89,9 +91,8 @@ export default function UserProfile() {
 
       {/* Logged-in user's predictions */}
       {authUser && (
-        <div className="mt-8 max-w-md mx-auto">
-          {/* <h3 className="text-xl font-bold mb-4">My Predictions</h3> */}
-          
+        <div>
+          <LeaderboardClient />
         </div>
       )}
     </div>
