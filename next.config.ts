58c62db342bcde2fae0890dd8060252ad866b012
Robert Cannon <rlacannon@gmail.com>
2025-12-09 12:48:40 +0100

    updates for other branch

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      ],
+    domains: [],
+  },
+  experimental: {
+    optimizeCss: false, // avoids LightningCSS binary issues on Netlify
   },
 };
 
